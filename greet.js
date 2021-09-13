6ecb12cdb87fd8fef0005e2a6c14c4f47caaa7d9
SiyaMndebele <siyamndebele303@gmail.com>
2021-09-13 12:41:34 +0200

    initial commit

--- greet.js
+++ greet.js
@@ -1,8 +1,8 @@
-// function greet(name){
-//     return "Hello, " + name;
-// }
-// var msg = greet('Xola')
-// console.log(msg)
+function greet(name){
+    return "Hello, " + name;
+}
+var msg = greet('Xola')
+console.log(msg)
 
 module.exports = function(name){
     return 'Hello, ' + name;
